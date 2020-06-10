d785295170b5e5ecc59706db8f5963687731b81f
jagerj97 <jagerj97@gmail.com>
2020-06-10 16:06:23 -0400

    V05

---
+++ Assets/Scripts/floorCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class floorCollision : MonoBehaviour
+{
+	 Vector3 originalPos;
+	 Quaternion originalRot;
+	 
+	 void Start()
+	{
+		originalPos = gameObject.transform.position;
+		originalRot = gameObject.transform.rotation;
+	}
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Floor")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            transform.position = originalPos;
+			transform.rotation = originalRot;
+        }
+    }
+}
